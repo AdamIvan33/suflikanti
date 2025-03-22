b7e15f54b3044807cc3ffebece15b5629dc42a85
AdamIvan33 <akiko.ivan.05@gmail.com>
2025-03-22 21:23:25 +0100

    Improved tests for n_power and n_root to only use natural exponents
    Based on feedback, changed the behaviour of some tests, I overlooked some cases of the previous tests.

--- src/math_lib_test.py
+++ src/math_lib_test.py
@@ -1,16 +1,19 @@
 import math_lib
+import pytest
 
 def test_n_power():
     assert math_lib.n_power(2, 3) == 8
     assert math_lib.n_power(-2, 3) == -8
-    assert math_lib.n_power(2, 0) == 1
     assert round(math_lib.n_power(2.5, 2), 5) == 6,25
+    with pytest.raises(ValueError, match="The exponent n must be a natural number"):
+        math_lib.n_power(2, 0)
 
 def test_n_root():
     assert math_lib.n_root(27, 3) == 3
     assert math_lib.n_root(16, 2) == 4
-    assert math_lib.n_root(2, 0) == 1
     assert round(math_lib.n_root(2, 2), 5) == 1,41421
+    with pytest.raises(ValueError, match="The exponent n must be a natural number"):
+        math_lib.n_root(2, 0)
 
 def test_modulo():
     assert math_lib.modulo(10, 3) == 1
