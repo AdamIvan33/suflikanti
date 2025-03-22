3cdf2cf99d0bcad2bdd12a59c9e00ce25ff9e62f
AdamIvan33 <akiko.ivan.05@gmail.com>
2025-03-22 20:41:38 +0100

    Added tests n_power, n_root, modulo, mockup of math_lib module
    Added were tests for functions from math_lib module. Test cases are for n_power, n_root and modulo functions. Also made a mockup of the math_lib module itself, so that the tests wouldn't return any errors regaring unknown functions.

---
+++ src/math_lib.py
@@ -0,0 +1,18 @@
+def add(number1, number2):
+    return
+def sub(number1, number2):
+    return
+def div(number1, number2):
+    return
+def mult(number1, number2):
+    return
+def absolute(number):
+    return
+def factorial(number):
+    return
+def modulo(number1, number2):
+    return
+def n_power(number1, number2):
+    return
+def n_root(number1, number2):
+    return
